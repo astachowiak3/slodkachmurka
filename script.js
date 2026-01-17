d32c3ae557f496d592305a8638bb0988caa33080
astachowiak3 <astachowiak3@edu.cdv.pl>
2026-01-17 19:51:48 +0100

    Dodanie script.js
    Animacje w javascipt

---
+++ script.js
@@ -0,0 +1,13 @@
+const animatedElements = document.querySelectorAll('.animate');
+
+
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+if (entry.isIntersecting) {
+entry.target.classList.add('show');
+}
+});
+});
+
+
+animatedElements.forEach(el => observer.observe(el));
